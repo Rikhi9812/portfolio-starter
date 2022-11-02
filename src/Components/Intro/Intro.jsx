85d55d305a9776322f1fb3233c5a11ae58dcf529
Rikhi9812 <rishi.rana606@gmail.com>
2022-11-02 23:01:23 +0900

    first-commit

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Intro.css';
+import Github from '../../img/github.png';
+import LinkedIn from '../../img/linkedin.png';
+import Instagram from '../../img/instagram.png';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import boy from '../../img/boy.png';
+// import thumbup from '../../img/thumbup.png';
+// import Crown from '../../img/crown.png';
+// import glassesimoji from '../../img/glassesimoji.png';
+
+function Intro() {
+  return (
+    <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+                <span>Hy! I Am</span>
+                <span>Andrew Thomas</span>
+                <span>Frontend Developer with high level of experience in web designing and development, producting the Quality work
+                </span>
+            </div>
+            <button className="button i-button">
+                Hire me
+            </button>
+            <div className="i-icons">
+                <img src={Github} alt="" />
+                <img src={LinkedIn} alt="" />
+                <img src={Instagram} alt="" />
+            </div>
+        </div>
+        <div className="i-right">
+                <img src={Vector1} alt="" />
+                <img src={Vector2} alt="" />
+                <img src={boy} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Intro
+
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="n-wrapper">
+        <div className="n-left">
+            <div className="n-name">Andrew</div>
+            <span>toggle</span>
+        </div>
+        <div className="n-right">
+            <div className="n-list">
+                <ul style={{listStyle: 'none'}}>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Experience</li>
+                    <li>Testimonials</li>
+                </ul>
+            </div>
+            <button className="button n-button">
+                Conctact us
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
